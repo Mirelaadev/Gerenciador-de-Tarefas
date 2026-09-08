defc61b5bcb102523e617492b7da2ab08c7aa571
Mirela Rodrigues <mirelaa.s.rodrigues@gmail.com>
2026-09-08 09:46:07 -0300

    feat: permite marcar tarefas como concluídas

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,37 @@ import AddTasks from "./components/AddTasks";
 import Tasks from "./components/Tasks"
 
 function App() {
-  const [tasks, setTasks] = useState([{
+  const [tasks, setTasks] = useState([
+    {
     id: 1,
     title: "Estudar programação",
     description: "Estudar para se tornar desenvolvedor full stack",
     isCompleted: false
+  },
+  {
+    id: 2,
+    title: "Estudar Ingles",
+    description: "Estudar para se tornar fluente",
+    isCompleted: false
+  },
+  {
+    id: 3,
+    title: "Estudar",
+    description: "Estudar para Aprender",
+    isCompleted: false
+  },
+]);
 
-  }]);
+function onTaskClick(taskId) {
+  const newTask = tasks.map(task => {
+    if (task.id === taskId) {
+      return {...task, isCompleted: !task.isCompleted}
+    }
+      return task
+  });
+  setTasks(newTask);
+
+}
 
   return(
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
@@ -18,7 +42,7 @@ function App() {
           Gerenciador de tarefas
           </h1>
           <AddTasks /> 
-          <Tasks tasks={tasks}/>
+          <Tasks tasks={tasks} onTaskClick={onTaskClick}/>
 
       </div>
     </div>
